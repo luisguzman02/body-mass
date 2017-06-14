0f44dd90d7d669f60d9084653ce06e377a498d3b
luisguzman02 <wero86@gmail.com>
2017-06-14 01:01:57 -0500

    Add config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :calculator, only: [:new] do
+    collection do
+      post :calculate
+      get :calculate, to: redirect('/calculator/new')
+    end
+  end
 end
